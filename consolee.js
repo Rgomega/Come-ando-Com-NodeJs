75eb572f03e6c34ecb0924296f5d3c3b17f4bb97
André_rgomegastly <96688972+Rgomega@users.noreply.github.com>
2022-09-07 19:32:05 -0300

    novo
    novo

---
+++ consolee.js
@@ -0,0 +1,14 @@
+// log -> registro
+
+const minhaVar = true;
+
+console.log(245)
+console.log("eu sou um texto")
+console.log(minhaVar)
+console.
+
+// tratamento de erro!
+
+//console.error("Deu erro!")
+console.log(new Error("deu erro"));
+console.error(new Error("deu erro"));
