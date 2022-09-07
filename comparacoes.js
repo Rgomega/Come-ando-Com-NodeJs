b254f9e6eba66e9074e8dcd031446c80421a504d
André_rgomegastly <96688972+Rgomega@users.noreply.github.com>
2022-09-07 20:02:16 -0300

    novo
    novoooo

---
+++ comparacoes.js
@@ -0,0 +1,18 @@
+//==(comparação implícita)
+
+const numero = 5;
+const text = "5";
+
+console.log( numero === text)
+
+//typeof
+console.log(typeof numero)
+console.log(typeof text)
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+//conversão explicita
+
+Number()
+String()
