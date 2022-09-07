90fca9567600ef4e7d167e97208832a8affcfe20
André_rgomegastly <96688972+Rgomega@users.noreply.github.com>
2022-09-07 09:23:25 -0300

    novos arquivos
    novos arquivos aqui

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos 
+
+//conversão implicita   
+ const numero = 456;
+ const numeroString= Number("456a");
+
+//Number()
+//String()
+
+ console.log(numero + numeroString)
+
+ 
+
+//conversão explicita
