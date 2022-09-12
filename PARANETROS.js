514abb2a392b38492128deabf736769514619c2c
André_rgomegastly <96688972+Rgomega@users.noreply.github.com>
2022-09-12 14:51:35 -0300

    Add files via upload

---
+++ PARANETROS.js
@@ -0,0 +1,31 @@
+
+
+
+              //2    //2
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+//console.log(soma(7 , 4))
+//console.log(soma(245 , 20))
+//console.log(soma(-500 , 60))
+
+// parâmetros x argumentos
+
+// ordem dos parâmetros
+
+//function nomeIdade(nome , idade) {
+//    return `meu nome é ${nome} e minha idade é ${idade}`
+//}
+
+
+//console.log(nomeIdade("André", 14))
+
+function multiplica(numero1 = 1, numero2 = 1){
+    return numero1 * numero2;
+}
+                         //9           //6
+console.log(multiplica(soma(4,5)))
+
+
+
---
+++ expressoes.js
@@ -0,0 +1,30 @@
+
+// declaraão de função
+
+//function minhaFuncao(param){
+    //bloco de codigo
+//}
+//minhaFuncao("param")
+
+//expressao de função
+
+
+
+
+//const soma = function(num1 ,num2){return num1 + num2}
+//console.log(soma(1, 1))
+
+// diferença principal???
+// funções e var são "listadas" no topo do arquivo
+
+console.log(apresentar())
+
+function apresentar(){
+    return " olá;"
+}
+
+console.log(soma(1, 1))
+const soma = function(num1 ,num2){return num1 + num2}
+
+
+
